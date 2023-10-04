86be2c38c2e8dd042a8d4a921040e2a84a682e88
lvlivingston <lvlivingston@gmail.com>
2023-10-03 20:01:22 -0500

    answer to Exercise 2

--- script.js
+++ script.js
@@ -10,3 +10,21 @@ function writeDing() {
 
 let timerId = setTimeout(writeDing, 3000);
 
+// Exercise 2
+
+// Javascript arrays have a built-in sort method that can take
+// a callback to tell it how to compare the things you want to sort.
+// Research the array sort method.
+// Write the sorting callback as a named function declaration
+// Write the callback function to provide to the sort method so that
+// the following code sorts the words from shortest to longest.
+// The sort method sorts "in place", that is, it modifies the array
+// Check that logging words now outputs
+// ["nice", "short", "medium", "lengthy", "delicious"]
+
+const words = ['short', 'medium', 'delicious', 'nice', 'lengthy'];
+
+words.sort(function(a, b) {
+    return a.length - b.length;
+});
+console.log(words)
