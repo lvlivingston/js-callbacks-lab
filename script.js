e512f9c46d42bfa974648b13354f24a846775833
lvlivingston <lvlivingston@gmail.com>
2023-10-03 20:24:46 -0500

    Answer to Exercise 5

--- script.js
+++ script.js
@@ -65,3 +65,49 @@ function log(elem, idx) {
   console.log(`Index: ${idx} / Element Value: ${elem}`);
 }
 forEach(colors, log);
+
+// Exercise 5
+
+
+/*
+The below functions are working asynchronous functions - DO NOT
+change any of their code. They are what we call "black boxes"
+because we do not need to know anything that goes on inside of them.
+Each of the three functions accept a single argument - a callback function.
+Write the code that invokes the three functions such that the output in the console will be:
+STEP 1 COMPLETE
+STEP 2 COMPLETE
+STEP 3 COMPLETE
+FINISHED
+Hints: 
+- Call `step1` first.
+- You cannot call `step2` until after `step1` has "finished", similarly, you cannot call `step3` until `step2` has "finished".
+- You must console.log the last line of the output, `FINISHED`, after `step3` has "finished".
+*/
+
+function step1(cb) {
+    setTimeout(function() {
+        console.log('STEP 1 COMPLETE');
+        cb()
+    }, 750);
+}
+function step2(cb) {
+    setTimeout(function() {
+        console.log('STEP 2 COMPLETE');
+        cb()
+    }, 500);
+}
+function step3(cb) {
+    setTimeout(function() {
+        console.log('STEP 3 COMPLETE');
+        cb()
+    }, 250);
+}
+function finished() {
+    console.log('FINISHED');
+}
+step1(function() {
+    step2(function() {
+        step3(finished);
+    });
+})
